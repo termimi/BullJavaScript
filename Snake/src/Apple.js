d66a64f042dba859bd00cfcba02b7417aff5c920
termimi <123395118+termimi@users.noreply.github.com>
2023-12-12 16:29:54 +0100

    ajout commentaire dans la classe pomme

--- Snake/src/Apple.js
+++ Snake/src/Apple.js
@@ -1,19 +1,24 @@
 import '../css/style.css';
 
 export class Apple{
+    // position x de la pomme
     appleX;
+    // position y de la pomme
     appleY;
     constructor(x,y){
         this.appleX = x;
         this.appleY = y;
     }
-
+    // dessine de la pomme
     drawApple(x,y,w,h) {
         let canvas = document.querySelector('canvas');
         let ctx = canvas.getContext('2d');
+        // couleur de la pomme
         ctx.fillStyle = 'red';
+        // position de la pomme
         ctx.fillRect(x, y, w, h)
     }
+    // détermination aléatoire de la position de la pomme
     randomApple(width) {
         this.appleX = Math.floor(Math.random() * 10) * width;
         this.appleY = Math.floor(Math.random() * 10) * width;
